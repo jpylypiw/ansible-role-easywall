c7a601330055f982748dfea470309076fe9533c6
jpylypiw <12394156+jpylypiw@users.noreply.github.com>
2020-06-21 23:48:13 +0200

    add ci/cd config

---
+++ .github/workflows/markdownlint.rb
@@ -0,0 +1,11 @@
+# for usage see https://github.com/marketplace/actions/run-markdownlint-mdl
+all
+
+# Don't enforce line length
+exclude_rule 'MD013'
+
+# exclude "Ordered list item prefix"
+exclude_rule 'MD029'
+
+# exclude "Multiple headers with the same content"
+exclude_rule 'MD024'
